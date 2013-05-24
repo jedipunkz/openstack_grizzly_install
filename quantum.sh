766d654216a38e1428901c8b1bc2e171b4ce8b7a
Tomokazu Hirai <tomokazu.hirai@kddi-web.com>
2013-05-24 11:07:38 +0900

    modified setup.conf.

--- quantum.sh
+++ quantum.sh
@@ -86,7 +86,7 @@ function create_network() {
             sleep 3
             INT_L3_AGENT_ID=$(quantum agent-list | grep ' L3 agent ' | get_field 1)
         done
-#        quantum l3-agent-router-add ${INT_L3_AGENT_ID} router-demo
+        #quantum l3-agent-router-add ${INT_L3_AGENT_ID} router-demo
         quantum router-interface-add ${INT_ROUTER_ID} ${INT_SUBNET_ID}
         # create external network
         EXT_NET_ID=$(quantum net-create --tenant-id ${TENANT_ID} ext_net -- --router:external=True | grep ' id ' | get_field 2)
